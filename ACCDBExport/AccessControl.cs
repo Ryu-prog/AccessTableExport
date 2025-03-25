c65d3b3a20b78ad917ab005d5714f4aecae5991e
Ryu <196807182+Ryu-prog@users.noreply.github.com>
2025-03-25 21:02:44 +0900

    プロジェクト ファイルを追加します。

---
+++ ACCDBExport/AccessControl.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Windows;
+
+using Access = Microsoft.Office.Interop.Access;
+
+namespace AccessTableExport
+{
+    internal class AccessControl
+    {
+        private const string Provider = "Microsoft.ACE.OLEDB.12.0";
+
+            public void ExportTable(string sourceDbPath, string dbPass, List<string> copyTableList, string destinationDbPath, string toDBPass = "")
+        {
+
+            string universalConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Jet OLEDB:Database Password={1};";
+
+            //コピー元からテーブルを取得する処理と
+            //コピー先にInsertする処理を分ける
+
+            //コピーデータセット
+            DataSet dsInsert = new DataSet();
+
+            // コピー元のデータベースからデータをデータセットとして取得
+            using (OleDbConnection sourceConnection = new OleDbConnection(String.Format(universalConnectionString, sourceDbPath, dbPass)))
+            {
+                sourceConnection.Open();
+
+                //コピー元からテーブルを取得する処理
+                foreach (string copyTable in copyTableList) {
+                    string selectQuery = $"SELECT * FROM {copyTable}"; // テーブル名を指定
+
+                    using (OleDbCommand selectCommand = new OleDbCommand(selectQuery, sourceConnection)) {
+                        using (OleDbDataAdapter adapter = new OleDbDataAdapter(selectCommand))
+                        {
+                            DataTable dataTable = new DataTable(copyTable);
+                            adapter.Fill(dataTable);
+                            dsInsert.Tables.Add(dataTable);
+                        }
+                    }
+                }
+
+                sourceConnection.Close();
+
+            }
+
+            //コピー先にInsertする処理
+
+            //sql成否フラグ
+            bool isSuccess = false;
+
+            string destinationConnectionString = String.Format(universalConnectionString, destinationDbPath, dbPass);
+
+            // コピー先のデータベースにデータを挿入
+            using (OleDbConnection destinationConnection = new OleDbConnection(destinationConnectionString))
+            {
+                //トランザクション開始
+                destinationConnection.Open();
+                OleDbTransaction TRN = destinationConnection.BeginTransaction();
+
+                try
+                {
+                    //sqlで更新する方法
+                    foreach (DataTable dtInsert in dsInsert.Tables)
+                    {
+                        //INSERT先のテーブルを全行削除
+                        using (OleDbCommand deleteCommand = new OleDbCommand($"DELETE FROM [{dtInsert.TableName}];", destinationConnection))
+                        {
+                            deleteCommand.Transaction = TRN;
+                            deleteCommand.ExecuteNonQuery();
+                        }
+
+                        // カラム名を自動生成
+                        IEnumerable<DataColumn> dcInsert = dtInsert.Columns.Cast<DataColumn>();
+
+                        string columns = string.Join(", ", dcInsert.Select(c => $"[{c.ColumnName}]"));
+                        string parameters = string.Join(", ", dcInsert.Select(c => "@" + c.ColumnName));
+
+                        //INSERT文を生成
+                        string insertQueryTemplate = $"INSERT INTO {dtInsert.TableName} ({columns}) VALUES ({parameters})";
+
+                        foreach (DataRow drInsert in dtInsert.Rows)
+                        {
+                            using (OleDbCommand insertCommand = new OleDbCommand(insertQueryTemplate, destinationConnection))
+                            {
+                                insertCommand.Transaction = TRN;
+                                foreach (DataColumn column in dtInsert.Columns)
+                                {
+                                    insertCommand.Parameters.AddWithValue("@" + column.ColumnName, drInsert[column]);
+                                }
+                                insertCommand.ExecuteNonQuery();
+                            }
+                        }
+                    }
+
+                    //トランザクションをコミット
+                    TRN.Commit();
+
+                    isSuccess = true;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message + Environment.NewLine + ex.ToString(), "エラー");
+                    MessageBox.Show("ロールバック", "コピー中にエラーが発生しました。ロールバックします。");
+
+                    try
+                    {
+                        TRN.Rollback();
+                        MessageBox.Show("ロールバック", "ロールバック成功");
+                    }
+                    catch (Exception ex2)
+                    {
+                        // This catch block will handle any errors that may have occurred
+                        // on the server that would cause the rollback to fail, such as
+                        // a closed connection.
+                        MessageBox.Show(ex2.Message + Environment.NewLine + ex2.ToString(), "エラー");
+                        MessageBox.Show("失敗", "ロールバック中にエラーが発生しました。");
+                    }
+
+                }
+                finally
+                {
+                    destinationConnection.Close();
+                }
+            }
+
+            if (isSuccess) MessageBox.Show("正常終了", "正常終了");
+
+        }
+
+        public object[] GetTableNames(string SheetInfoPath, string DBPass) {
+            var builder = new System.Data.OleDb.OleDbConnectionStringBuilder();
+            builder["Provider"] = Provider;
+            builder["Data Source"] = SheetInfoPath;
+            builder["Jet OLEDB:Database Password"] = DBPass;
+
+            DataTable dt;
+
+            using (var conn = new System.Data.OleDb.OleDbConnection(builder.ConnectionString))
+            {
+                conn.Open();
+                dt = conn.GetSchema("Tables");
+                conn.Close();
+            }
+
+            DataTable TypeTable = dt.Select("[TABLE_TYPE] = 'TABLE'").CopyToDataTable();
+
+            object[] TableNames = TypeTable.AsEnumerable().Select(i => i["TABLE_NAME"]).ToArray();
+
+            return TableNames;
+        }
+    }
+}
---
+++ ACCDBExport/MainWindow.xaml.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace AccessTableExport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.DBPassword) == false)
+            {
+                this.pbDBPass.Password = Properties.Settings.Default.DBPassword;
+            }
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.TableListPath) == false && System.IO.Path.Exists(Properties.Settings.Default.TableListPath)) {
+                this.tblTXTPath.Text = Properties.Settings.Default.TableListPath;
+            }
+
+            Closing += FormMain_FormClosing;
+        }
+
+        /// <summary>
+        /// アプリ終了時のプロパティ保存
+        /// </summary>
+        private void FormMain_FormClosing(object sender, CancelEventArgs e)
+        {
+            Properties.Settings.Default.DBFolder = this.tbxDBFilePath.Text;
+            Properties.Settings.Default.DBPassword = this.pbDBPass.Password;
+            Properties.Settings.Default.TableListPath = this.tblTXTPath.Text;
+            Properties.Settings.Default.ToDBFolder = this.tbxToDBFilePath.Text;
+            Properties.Settings.Default.Save();
+        }
+
+
+        private void DBSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            opd.Filter = "データベース(*.accdb)|*.accdb";
+
+            if (!String.IsNullOrEmpty(Properties.Settings.Default.DBFolder))
+            {
+                opd.InitialDirectory = System.IO.Path.GetDirectoryName(Properties.Settings.Default.DBFolder);
+                opd.FileName = System.IO.Path.GetFileName(Properties.Settings.Default.DBFolder);
+            }
+
+            if (opd.ShowDialog() == true)
+            {
+                Properties.Settings.Default.DBFolder = opd.FileName;
+                Properties.Settings.Default.Save();
+                this.tbxDBFilePath.Text = opd.FileName;
+                //this.setSheetTypeCmb();
+
+                this.setTableList();
+
+                if (string.IsNullOrEmpty(this.tblTXTPath.Text) == false) {
+                    this.selectTableList(this.tblTXTPath.Text);
+                }
+            }
+        }
+
+        private void setTableList() {
+
+            AccessControl AccessTable = new AccessControl();
+            object[] tableNames = AccessTable.GetTableNames(this.tbxDBFilePath.Text, this.pbDBPass.Password);
+
+            foreach (object tableName in tableNames) {
+                this.TableList.Items.Add(tableName.ToString());
+            }
+        }
+
+        private void selectTableList(string tblTXTPath)
+        {
+            TableList.SelectedItems.Clear();
+
+            // 読み込みたいTXTファイルのパスを指定して開く
+            StreamReader sr = new StreamReader(@tblTXTPath);
+
+            while (!sr.EndOfStream) {
+                // TXTファイルの一行を読み込む
+                string line = sr.ReadLine();
+
+                foreach(string tableItem in TableList.Items) 
+                {
+                    if (tableItem == line)
+                    {
+                        TableList.SelectedItems.Add(tableItem);
+                    }
+                }
+
+            }
+            sr.Close();
+        }
+
+        private void TXTSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            opd.Filter = "テーブル一覧ファイル(*.txt)|*.txt";
+
+            if (!String.IsNullOrEmpty(Properties.Settings.Default.TableListPath))
+            {
+                opd.InitialDirectory = System.IO.Path.GetDirectoryName(Properties.Settings.Default.TableListPath);
+                opd.FileName = System.IO.Path.GetFileName(Properties.Settings.Default.TableListPath);
+            }
+
+            if (opd.ShowDialog() == true)
+            {
+                this.tblTXTPath.Text = opd.FileName;
+
+                this.selectTableList(this.tblTXTPath.Text);
+            }
+        }
+
+        private void TXTClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.tblTXTPath.Text = string.Empty;
+        }
+
+        private void ToDBSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            opd.Filter = "データベース(*.accdb)|*.accdb";
+
+            if (!String.IsNullOrEmpty(Properties.Settings.Default.DBFolder))
+            {
+                opd.InitialDirectory = System.IO.Path.GetDirectoryName(Properties.Settings.Default.ToDBFolder);
+                opd.FileName = System.IO.Path.GetFileName(Properties.Settings.Default.ToDBFolder);
+            }
+
+            if (opd.ShowDialog() == true)
+            {
+                Properties.Settings.Default.ToDBFolder = opd.FileName;
+                Properties.Settings.Default.Save();
+                this.tbxToDBFilePath.Text = opd.FileName;
+            }
+        }
+
+        private void CopyExistButton_Click(object sender, RoutedEventArgs e)
+        {
+            //コピー対象のテーブルをまとめる
+            List<string> CopyTableList = new List<string>();
+
+            foreach (string oneItemLine in this.TableList.SelectedItems)
+            {
+                CopyTableList.Add(oneItemLine);
+            }
+
+            //string[] CopyTableLists = this.TableList.SelectedItems;
+
+            AccessControl AccessTable = new AccessControl();
+
+            AccessTable.ExportTable(this.tbxDBFilePath.Text, this.pbDBPass.Password, CopyTableList, this.tbxToDBFilePath.Text);
+
+           
+        }
+    }
+}
